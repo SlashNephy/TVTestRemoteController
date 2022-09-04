0875898a80923260be302677fdccda63dc69e50f
SlashNephy <spica@starry.blue>
2022-09-04 23:29:59 +0900

    ひな形作成

---
+++ pch.h
@@ -0,0 +1,12 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <format>
+#include <vector>
+
+#include "TVTestPlugin.h"
+
+#endif
---
+++ plugin.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+
+class CRemoteControllerPlugin final : public TVTest::CTVTestPlugin
+{
+public:
+    DWORD GetVersion() override
+    {
+        // TVTest ver.0.9.0 or later
+        return TVTEST_PLUGIN_VERSION_(0, 0, 14);
+    }
+
+    bool GetPluginInfo(TVTest::PluginInfo* pInfo) override
+    {
+        pInfo->Type = TVTest::PLUGIN_TYPE_NORMAL;
+        pInfo->Flags = 0;
+        pInfo->pszPluginName = L"Remote Controller";
+        pInfo->pszCopyright = L"© 2022 SlashNephy";
+        pInfo->pszDescription = L"Web ブラウザ経由のリモコンを提供するプラグインです。";
+
+        return true;
+    }
+
+    bool Initialize() override
+    {
+        return true;
+    }
+
+    bool Finalize() override
+    {
+        return true;
+    }
+
+private:
+    HANDLE queue = nullptr;
+    std::vector<std::wstring> urls = std::vector{
+        std::wstring(L""),
+    };
+};
